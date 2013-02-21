1243b1fd41cfe28732676abb81e353a0f1a5fb0d
Joshua Dance <joshua.dance@gmail.com>
2013-02-21 16:16:38 -0700

    Made all the changes for lecture 3

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Joshua Dance on 2/21/13.
+//  Copyright (c) 2013 Joshua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Joshua Dance on 2/21/13.
+//  Copyright (c) 2013 Joshua. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
